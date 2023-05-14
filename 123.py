4af92c8da04619af2ebc71427cfb42e85ba8c7e9
hojin Lim <torl123491@gmail.com>
2023-05-14 22:20:19 +0900

    second commit

--- 123.py
+++ 123.py
@@ -3,7 +3,7 @@ import google.auth
 from googleapiclient.discovery import build
 
 # API 키 설정
-api_key = 'AIzaSyCV0mY6gLO8iyUfl5A5s9hMYvU2I4YnTX0'
+api_key = ''
 
 # API 클라이언트 생성
 youtube = build('youtube', 'v3', developerKey=api_key)
